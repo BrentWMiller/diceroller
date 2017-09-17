57d900fa9d7e3523314e5932c84008fdd2a7efc5
Brent Miller <brentwmiller93@gmail.com>
2017-09-17 18:36:31 -0400

    Setups base roller component

---
+++ src/app/dice-roller/dice-roller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dr-dice-roller',
+  templateUrl: './dice-roller.component.html',
+  styleUrls: ['./dice-roller.component.scss']
+})
+export class DiceRollerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
