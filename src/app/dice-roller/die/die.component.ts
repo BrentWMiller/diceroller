de67d4afbdcccb6e2d8db05bfa611a7925aa8fcd
Brent Miller <brentwmiller93@gmail.com>
2017-09-17 18:36:43 -0400

    Sets up individual die component

---
+++ src/app/dice-roller/die/die.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dr-die',
+  templateUrl: './die.component.html',
+  styleUrls: ['./die.component.scss']
+})
+export class DieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
