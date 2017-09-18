498033a8c05f2b1179e9631ca508037e27037cfa
Brent Miller <brentwmiller93@gmail.com>
2017-09-17 20:28:48 -0400

    adds roller component

--- src/app/dice-roller/dice-roller.component.ts
+++ src/app/dice-roller/dice-roller.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { dice } from './data/dice.data';
+import { Die } from './models/die.interface';
+
 @Component({
-  selector: 'dr-dice-roller',
-  templateUrl: './dice-roller.component.html',
-  styleUrls: ['./dice-roller.component.scss']
+	selector: 'dr-dice-roller',
+	templateUrl: './dice-roller.component.html',
+	styleUrls: ['./dice-roller.component.scss']
 })
 export class DiceRollerComponent implements OnInit {
 
-  constructor() { }
+	dice: Die[];
 
-  ngOnInit() {
-  }
+	constructor() {
+	}
+
+	ngOnInit() {
+		this.dice = dice;
+	}
 
 }
