8db884a0faf2148a7c79444c99603d7aab87bbe7
Brent Miller <brentwmiller93@gmail.com>
2017-09-17 20:28:11 -0400

    sets up dice data

---
+++ src/app/dice-roller/data/dice.data.ts
@@ -0,0 +1,11 @@
+import { Die } from '../models/die.interface';
+
+export const dice: Die[] = [
+	{ type: 4 },
+	{ type: 6 },
+	{ type: 8 },
+	{ type: 10 },
+	{ type: 12 },
+	{ type: 20 },
+	{ type: 100 },
+]
