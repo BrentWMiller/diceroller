e249e4ccbec5cdfdc6c3aef8933b7a094ba7ecc6
Brent Miller <brentwmiller93@gmail.com>
2017-09-17 20:28:27 -0400

    Sets up die component

--- src/app/dice-roller/die/die.component.ts
+++ src/app/dice-roller/die/die.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Die } from '../models/die.interface';
 
 @Component({
-  selector: 'dr-die',
-  templateUrl: './die.component.html',
-  styleUrls: ['./die.component.scss']
+	selector: 'dr-die',
+	templateUrl: './die.component.html',
+	styleUrls: ['./die.component.scss']
 })
 export class DieComponent implements OnInit {
+	@HostBinding('class') dieClass = 'die';
 
-  constructor() { }
+	@Input()
+	die: Die[];
 
-  ngOnInit() {
-  }
+	counts: number[];
+
+	constructor() { }
+
+	ngOnInit() {
+		this.counts = [1, 2, 3, 4, 5];
+	}
 
 }
