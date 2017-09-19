a74885d8066de3112baad8dabcc488a79b646474
Brent Miller <brentwmiller93@gmail.com>
2017-09-18 21:56:48 -0400

    updates die data

--- src/app/dice-roller/data/dice.data.ts
+++ src/app/dice-roller/data/dice.data.ts
@@ -1,11 +1,11 @@
 import { Die } from '../models/die.interface';
 
 export const dice: Die[] = [
-	{ type: 4 },
-	{ type: 6 },
-	{ type: 8 },
-	{ type: 10 },
-	{ type: 12 },
-	{ type: 20 },
-	{ type: 100 },
+	{ name: 'd4', value: 4 },
+	{ name: 'd6', value: 6 },
+	{ name: 'd8', value: 8 },
+	{ name: 'd10', value: 10 },
+	{ name: 'd12', value: 12 },
+	{ name: 'd20', value: 20 },
+	{ name: 'd100', value: 100 },
 ]
