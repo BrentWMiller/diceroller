e97b6bd8e5030fdcfca99acbeb42d1d942bd5b98
Brent Miller <brentwmiller93@gmail.com>
2017-09-25 21:18:27 -0400

    Passes die count up to parent

--- src/app/dice-roller/die-count/die-count.component.ts
+++ src/app/dice-roller/die-count/die-count.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'dr-die-count',
-  templateUrl: './die-count.component.html',
-  styleUrls: ['./die-count.component.scss']
+	selector: 'dr-die-count',
+	templateUrl: './die-count.component.html',
+	styleUrls: ['./die-count.component.scss']
 })
 export class DieCountComponent implements OnInit {
 
-  constructor() { }
+	@Output()
+	count: EventEmitter<number> = new EventEmitter<number> ();
 
-  ngOnInit() {
-  }
+	value: number;
+
+	constructor() { }
+
+	ngOnInit() {
+		this.onAmountChange(1);
+	}
+
+	onAmountChange(value: number): void {
+		if (this.value === 0 || this.value === null) {
+			this.value = 1;
+		}
+		
+		this.value = value;
+		this.count.emit(this.value);
+	}
 
 }
